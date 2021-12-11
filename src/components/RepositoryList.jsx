738dee4d6f0673b3ff7039c39715fc1d6ab8637e
Thales Domingues <thdomingues.dev@gmail.com>
2021-12-10 22:43:06 -0300

    feat: call the api to fetch repositories data

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,5 @@
 // Packages
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 // Components
 import RepositoryItem from './RepositoryItem'
@@ -7,18 +7,33 @@ import RepositoryItem from './RepositoryItem'
 // Styles
 import '../styles/repositories.scss'
 
-const RepositoryList = () => (
-  <section className="repository-list">
-    <h1>Lista de Repositórios</h1>
+//https://api.github.com/users/thdomingues-dev/repos
 
-    <ul>
-      <RepositoryItem
-        name="My Github"
-        description="Some thing here"
-        link="https://github.com/thdomingues-dev"
-        />
-    </ul>
-  </section>
-)
+const RepositoryList = () => {
+  const [repositories, setRepositories] = useState([])
+
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/thdomingues-dev/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        {repositories.map(repository => 
+          <RepositoryItem
+            key={repository?.name}
+            name={repository?.name}
+            description={repository?.description ? repository?.description : 'No description'}
+            link={repository?.html_url}
+          />
+        )}
+      </ul>
+    </section>
+)}
 
 export default RepositoryList
